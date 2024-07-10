ecba1721a6b7551e791b0c04e5cd6bc7e57a2412
Wali Ullah Milton <waliullahmiltoncse@gmail.com>
2024-07-10 08:29:27 +0600

    userSignUp dispatch

--- frontend/src/components/SignUp.jsx
+++ frontend/src/components/SignUp.jsx
@@ -1,68 +1,49 @@
-import {useState } from 'react'
+import { useState } from 'react'
 import Input from './Input'
 import Button from './Btn'
-import axios from "axios"
+// import axios from "axios"
 import { IoClose } from "react-icons/io5";
+import { useDispatch } from 'react-redux';
+import  { userSignUp } from '../features/reducers/userSlice';
 
 const SignUp = ({onClick}) => {
-    const [name,setName]=useState("")
-    const [email,setEmail]=useState("")
-    const [phone,setPhone]=useState("")
-    const [address,setAddress]=useState("")
-    const [password,setPassword]=useState("")
-    const handleSignup = async()=>{
-        try {
-            const data = await axios.post("http://localhost:3000/registration",{
-                name : name,
-                email : email,
-                phone : phone,
-                address : address,
-                password : password
-            })
-            if(data){
-                setName("");
-                setEmail("");
-                setPhone("");
-                setAddress("");
-                setPassword("");
-            }
-        } catch (error) {
-            console.log(error)
-        }
+    // const [name,setName]=useState("")
+    // const [email,setEmail]=useState("")
+    // const [phone,setPhone]=useState("")
+    // const [address,setAddress]=useState("")
+    // const [password,setPassword]=useState("")
+    const [formData,setFormData] = useState({
+        name : "",
+        email : "",
+        phone : "",
+        address : "",
+        password : ""
+    })
+    console.log(formData)
+    const handleInputChange = (e)=>{
+        setFormData({...formData,[e.target.name] : e.target.value})
     }
+    const dispatch = useDispatch()
+    const handleSignup = ()=>{
+        // const {name,email,phone,address,password} = formData;
+        const { name, email, phone, address, password } = formData;
+        dispatch(userSignUp({ name, email, phone, address, password }));
+    }
+
   return (
     <section className='absolute top-10 right-[50%] translate-x-[50%] z-50 px-20 transition-all ease-in-out duration-300'>
-        <div className='bg-slate-800 px-8 py-12 relative'>
-            <span className='absolute right-2 top-2 text-white text-2xl'
-            onClick={onClick}
-            ><IoClose/></span>
-        <ul className='flex flex-col gap-6 items-center'>
-            <Input placeholder="name"
-            value={name}
-            onChange={(e)=>setName(e.target.value)}
-            />
-            <Input placeholder="email"
-            value={email}
-            onChange={(e)=>setEmail(e.target.value)}
-            />
-            <Input placeholder="phone"
-            value={phone}
-            onChange={(e)=>setPhone(e.target.value)}
-            />
-            <Input placeholder="address"
-            value={address}
-            onChange={(e)=>setAddress(e.target.value)}
-            />
-            <Input placeholder="password"
-            value={password}
-            onChange={(e)=>setPassword(e.target.value)}
-            />
-            <Button className="max-w-[200px]" text="sign up"
-            onClick={handleSignup}
-            />
-        </ul>
-        </div>
-    </section>
+            <div className='bg-slate-800 px-8 py-12 relative'>
+                <span className='absolute right-2 top-2 text-white text-2xl' onClick={onClick}><IoClose /></span>
+                <ul className='flex flex-col gap-6 items-center'>
+                    <Input placeholder="name" name="name" value={formData.name} onChange={handleInputChange} />
+                    <Input placeholder="email" name="email" value={formData.email} onChange={handleInputChange} />
+                    <Input placeholder="phone" name="phone" value={formData.phone} onChange={handleInputChange} />
+                    <Input placeholder="address" name="address" value={formData.address} onChange={handleInputChange} />
+                    <Input placeholder="password" name="password" value={formData.password} onChange={handleInputChange} />
+                    <Button className="max-w-[200px]" text="sign up" onClick={handleSignup} />
+                </ul>
+            </div>
+        </section>
   )
 }
 
